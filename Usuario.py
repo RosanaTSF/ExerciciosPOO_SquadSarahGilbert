438c3f071bfaf5d905297e4976e5e7a6fcded528
Lívia Boscolo <132012400+liviaboscolo@users.noreply.github.com>
2024-08-18 19:31:00 -0300

    Update Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -6,15 +6,11 @@ rd = random.Random()
 rd.seed(123) 
 
 class Usuario(Pessoa):
-    def __init__(self, nome,telefone,nacionalidade):
-        super().__init__(nome)
+    def __init__(self, id, nome, email, telefone, nacionalidade):
+        super().__init__(nome, id, email)
         self.telefone=telefone
         self.nacionalidade=nacionalidade
-        self.__id = str(uuid.UUID(int=rd.getrandbits(128), version=4))
-
+      
     def __repr__(self):
         return f'Usuario | ID: {self.__id}, Nome: {self.nome}, Telefone: {self.telefone}, Nacionaldiade: {self.nacionalidade}\n'
-    
-    @property
-    def id(self):
-        return self.__id
+
